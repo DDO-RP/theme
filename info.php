a7690feccfb6a96ac5bc12dcfa843fa2238a736e
Jay Copeland <rjcopeland@users.noreply.github.com>
2016-08-15 19:26:37 +0100

    Add files via upload

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php
+
+$info['name'] = 'DDO RP';
+$info['description'] = 'The official DDO RP site theme';
+$info['version'] = '1.0';
+$info['parent_theme']='default';
+$info['author'] = "RJ Copeland";
+$info['author_url'] = 'http://www.rjcopeland.ga';
+$info['license'] = 'Core License';
+$info['core'] = '1.x';
